a0b86cf77dae6839789f01517cca500e5b6615de
Anthony <maiorania@gmail.com>
2023-10-24 13:16:48 -0500

    fingerprint

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 export PASSIVE="0"
+export FINGERPRINT="HONDA ACCORD 2018"
 echo -en "0.2.0" > /data/params/d/CompletedTrainingVersion
 echo -en "2" > /data/params/d/HasAcceptedTerms
 exec ./launch_chffrplus.sh
