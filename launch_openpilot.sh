a300eb8d256e58cd4a0bb006c27a04c9d1d9ab56
Anthony <maiorania@gmail.com>
2023-10-24 13:20:26 -0500

    api

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -4,4 +4,5 @@ export PASSIVE="0"
 export FINGERPRINT="HONDA ACCORD 2018"
 echo -en "0.2.0" > /data/params/d/CompletedTrainingVersion
 echo -en "2" > /data/params/d/HasAcceptedTerms
+echo -en "1" > /data/params/d/dp_api_custom
 exec ./launch_chffrplus.sh
